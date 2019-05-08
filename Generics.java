b71b1830ba53428210b3f68d390f869e735b0cfc
alan toro <alanprog@gmail.com>
2019-05-08 10:18:11 +0200

    Update Generics.java

--- Generics.java
+++ Generics.java
@@ -1,10 +1,15 @@
 
 public <T> T mymethod(T type){
-  return type;
+    return type;
 }
 
 public <T extends YourType> T mymethod(T type){
-  // now you can use YourType methods
-  return type;
+    // now you can use YourType methods
+    return type;
 }
 
+public static <T extends Comparable<T>> int compare(final T name1, final T name2) {
+    if (name1 == null) return name2 == null ? 0 : -1;
+    if (name2 == null) return 1;
+    return name1.compareTo(name2);
+}
